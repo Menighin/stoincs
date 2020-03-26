704b2f6d3e5827d07979a4109d3ae95cef7ebd35
Menighin <joao.menighin@gmail.com>
2020-03-26 19:30:58 -0300

    Design - In Progress

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,28 @@
 <template>
     <q-page class="flex flex-center">
-        <img alt="Quasar logo" src="~assets/snout.svg">
+        <img alt="Quasar logo" src="~assets/snout.svg" v-show="false">
+        <inline-svg
+            src="img/snout.svg"
+            fill="black"
+            style="transform: scale(2)"
+            aria-label="Porquinho Digital Logo"
+        ></inline-svg>
     </q-page>
 </template>
 
 <script>
+import InlineSvg from 'vue-inline-svg';
+
 export default {
-    name: 'PageIndex'
+    name: 'PageIndex',
+    components: {
+        InlineSvg
+    }
 };
 </script>
+
+<style lang="scss">
+    #pig-snout-svg {
+        transition: transform .5s;
+    }
+</style>
